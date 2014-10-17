c0ab0519a1815c01b096f5d3e66d55c81b4c52c4
Dmitri <dimi615@pisem.net>
2014-10-17 17:56:47 +0400

    Added windows-build-qt-static.ps1

---
+++ QTstatic/windows-build-qt-static.ps1
@@ -0,0 +1,259 @@
+#-----------------------------------------------------------------------------
+# 
+#  Copyright (c) 2013, Thierry Lelegard
+#  All rights reserved.
+#  Tweaked by DimiG, 2014
+# 
+#-----------------------------------------------------------------------------
+
+<#
+ .SYNOPSIS
+
+  Build a static version of Qt for Windows.
+
+ .DESCRIPTION
+
+  This scripts downloads Qt source code, compiles and installs a static version
+  of Qt. It assumes that a prebuilt Qt / MinGW environment is already installed,
+  typically in C:\Qt. This prebuilt environment uses shared libraries. It is
+  supposed to remain the main development environment for Qt. This script adds
+  a static version of the Qt libraries in order to allow the construction of
+  standalone and self-sufficient executable.
+
+  This script is typically run from the Windows Explorer.
+
+  Requirements:
+  - Windows PowerShell 3.0 or higher.
+  - 7-zip.
+
+ .PARAMETER QtSrcUrl
+
+  URL of the Qt source file archive.
+  By default, use the latest identified version.
+
+ .PARAMETER QtStaticDir
+
+  Root directory where the static versions of Qt are installed.
+  By default, use C:\Qt\Static.
+
+ .PARAMETER QtVersion
+
+  The Qt version. By default, this script tries to extract the version number
+  from the Qt source file name.
+
+ .PARAMETER MingwDir
+
+  Root directory of the MinGW prebuilt environment. By default, use the version
+  which was installed by the prebuilt Qt environment.
+
+ .PARAMETER NoPause
+
+  Do not wait for the user to press <enter> at end of execution. By default,
+  execute a "pause" instruction at the end of execution, which is useful
+  when the script was run from Windows Explorer.
+#>
+
+# Change the $QtSrcUrl to the version you prefer to compile (MUST BE AGREE WITH INSTALLED Dynamic pre-built Qt)
+[CmdletBinding()]
+param(
+    $QtSrcUrl = "http://download.qt-project.org/official_releases/qt/5.3/5.3.2/single/qt-everywhere-opensource-src-5.3.2.7z",
+    $QtStaticDir = "C:\Qt\Static",
+    $QtVersion = "",
+    $MingwDir = "",
+    [switch]$NoPause = $false
+)
+
+# PowerShell execution policy.
+Set-StrictMode -Version 3
+
+#-----------------------------------------------------------------------------
+# Main code
+#-----------------------------------------------------------------------------
+
+function Main
+{
+    # Check that 7zip is installed. We use it to expand the downloaded archive.
+    [void] (Get-7zip)
+
+    # Get Qt source file name from URL.
+    $QtSrcFileName = Split-Path -Leaf $QtSrcUrl
+
+    # If Qt version is not specified on the command line, try to extract the value.
+    if (-not $QtVersion) {
+        $QtVersion = $QtSrcFileName -replace "`.[^`.]*$",''
+        $QtVersion = $QtVersion -replace 'qt-',''
+        $QtVersion = $QtVersion -replace 'everywhere-',''
+        $QtVersion = $QtVersion -replace 'opensource-',''
+        $QtVersion = $QtVersion -replace 'src-',''
+        $QtVersion = $QtVersion -replace '-src',''
+    }
+    Write-Output "Building static Qt version $QtVersion"
+
+    # Qt installation directory.
+    $QtDir = "$QtStaticDir\$QtVersion"
+
+    # Get MinGW root directory, if not specified on the command line.
+    if (-not $MingwDir) {
+        # Search all instances of gcc.exe from C:\Qt prebuilt environment.
+        # Incorrect gcc.exe location in the code (SET UP correct one for your location)
+        # The original was: $GccList = @(Get-ChildItem -Path C:\Qt\*\Tools\mingw*\bin\gcc.exe | ForEach-Object FullName | Sort-Object)
+        $GccList = @(Get-ChildItem -Path C:\Qt\Tools\mingw482_32\bin\gcc.exe | ForEach-Object FullName | Sort-Object)
+        if ($GccList.Length -eq 0) {
+            Exit-Script "MinGW environment not found, no Qt prebuilt version?"
+        }
+        $MingwDir = (Split-Path -Parent (Split-Path -Parent $GccList[$GccList.Length - 1]))
+    }
+    Write-Output "Using MinGW from $MingwDir"
+
+    # Build the directory tree where the static version of Qt will be installed.
+    Create-Directory $QtStaticDir\src
+    Create-Directory $QtDir
+
+    # Download the Qt source package if not yet done.
+    Download-File $QtSrcUrl $QtStaticDir\src\$QtSrcFileName
+
+    # Directory of expanded packages.
+    $QtSrcDir = "$QtStaticDir\src\$((Get-Item $QtStaticDir\src\$QtSrcFileName).BaseName)"
+
+    # Expand archives if not yet done
+    Expand-Archive $QtStaticDir\src\$QtSrcFileName $QtStaticDir\src $QtSrcDir
+
+    # Patch Qt's mkspecs for static build.
+    $File = "$QtSrcDir\qtbase\mkspecs\win32-g++\qmake.conf"
+    if (-not (Select-String -Quiet -SimpleMatch -CaseSensitive "# [QT-STATIC-PATCH]" $File)) {
+        Write-Output "Patching $File ..."
+        Copy-Item $File "$File.orig"
+        @"
+
+# [QT-STATIC-PATCH]
+QMAKE_LFLAGS += -static -static-libgcc
+QMAKE_CFLAGS_RELEASE -= -O2
+QMAKE_CFLAGS_RELEASE += -Os -momit-leaf-frame-pointer
+DEFINES += QT_STATIC_BUILD
+"@ | Out-File -Append $File -Encoding Ascii
+    }
+
+    # Set a clean path including MinGW.
+    $env:Path = "$MingwDir\bin;$MingwDir\opt\bin;$env:SystemRoot\system32;$env:SystemRoot"
+
+    # Force English locale to avoid weird effects of tools localization.
+    $env:LANG = "en"
+
+    # Set environment variable QT_INSTALL_PREFIX. Documentation says it should be
+    # used by configure as prefix but this does not seem to work. So, we will
+    # also specify -prefix option in configure.
+    $env:QT_INSTALL_PREFIX = $QtDir
+
+    # Configure, compile and install Qt.
+    Push-Location $QtSrcDir
+    cmd /c "configure.bat -static -debug-and-release -platform win32-g++ -prefix $QtDir `        -qt-zlib -qt-pcre -qt-libpng -qt-libjpeg -qt-freetype -opengl desktop -qt-sql-sqlite -no-openssl `        -opensource -confirm-license `        -make libs -nomake tools -nomake examples -nomake tests"
+    mingw32-make -k -j4
+    mingw32-make -k install
+    Pop-Location
+
+    # Patch Qt's installed mkspecs for static build of application.
+    $File = "$QtDir\mkspecs\win32-g++\qmake.conf"
+    @"
+CONFIG += static
+"@ | Out-File -Append $File -Encoding Ascii
+
+    Exit-Script
+}
+
+#-----------------------------------------------------------------------------
+# A function to exit this script. The Message parameter is used on error.
+#-----------------------------------------------------------------------------
+
+function Exit-Script ([string]$Message = "")
+{
+    $Code = 0
+    if ($Message -ne "") {
+        Write-Output "ERROR: $Message"
+        $Code = 1
+    }
+    if (-not $NoPause) {
+        pause
+    }
+    exit $Code
+}
+
+#-----------------------------------------------------------------------------
+# Silently create a directory.
+#-----------------------------------------------------------------------------
+
+function Create-Directory ([string]$Directory)
+{
+    [void] (New-Item -Path $Directory -ItemType "directory" -Force)
+}
+
+#-----------------------------------------------------------------------------
+# Download a file if not yet present.
+# Warning: If file is present but incomplete, do not download it again.
+#-----------------------------------------------------------------------------
+
+function Download-File ([string]$Url, [string]$OutputFile)
+{
+    $FileName = Split-Path $Url -Leaf
+    if (-not (Test-Path $OutputFile)) {
+        # Local file not present, start download.
+        Write-Output "Downloading $Url ..."
+        try {
+            $webclient = New-Object System.Net.WebClient
+            $webclient.DownloadFile($Url, $OutputFile)
+        }
+        catch {
+            # Display exception.
+            $_
+            # Delete partial file, if any.
+            if (Test-Path $OutputFile) {
+                Remove-Item -Force $OutputFile
+            }
+            # Abort
+            Exit-Script "Error downloading $FileName"
+        }
+        # Check that the file is present.
+        if (-not (Test-Path $OutputFile)) {
+            Exit-Script "Error downloading $FileName"
+        }
+    }
+}
+
+#-----------------------------------------------------------------------------
+# Get path name of 7zip, abort if not found.
+#-----------------------------------------------------------------------------
+
+function Get-7zip
+{
+    # Change the PATH to your 7z program
+    # The original was: $Exe = "C:\Program Files\7-Zip\7z.exe"
+    $Exe = "C:\bin\7za.exe"
+    if (-not (Test-Path $Exe)) {
+        $Exe = "C:\Program Files (x86)\7-Zip\7z.exe"
+    }
+    if (-not (Test-Path $Exe)) {
+        Exit-Script "7-zip not found, install it first, see http://www.7-zip.org/"
+    }
+    $Exe
+}
+
+#-----------------------------------------------------------------------------
+# Expand an archive file if not yet done.
+#-----------------------------------------------------------------------------
+
+function Expand-Archive ([string]$ZipFile, [string]$OutDir, [string]$CheckFile)
+{
+    # Check presence of expected expanded file or directory.
+    if (-not (Test-Path $CheckFile)) {
+        Write-Output "Expanding $ZipFile ..."
+        & (Get-7zip) x $ZipFile "-o$OutDir" | Select-String -Pattern "^Extracting " -CaseSensitive -NotMatch
+        if (-not (Test-Path $CheckFile)) {
+            Exit-Script "Error expanding $ZipFile, $OutDir\$CheckFile not found"
+        }
+    }
+}
+
+#-----------------------------------------------------------------------------
+# Execute main code.
+#-----------------------------------------------------------------------------
+
+. Main
--- README.md
+++ README.md
@@ -2,14 +2,25 @@ myQTApps
 ========
 This repository dedicated to myOWN QT C++ programs I use for tests or daily purposes.
 
-These programs created in Qt Creator 2.8.1 which based on Qt 5.1.1 (MSVC 2010, 32 bit).
+These programs created in Qt Creator 3.2.2 which based on Qt 5.3 (MSVC 2010, 32 bit) or Qt 5.3 (MinGW 4.8.2, 32 bit).
 Repository contain Qt Creator project which you're able to clone and open in your Qt Creator version.
 Compile it and use it.
 
 Codes description
 -------------------
 
-### myCSharpApps folder
+### myQTApps folder
+
+* `QTstatic`: This folder with PowerShell [script][QtlMovie] inside downloads a Qt source code, compiles and installs a static version of Qt.<br>
+   This script adds a static version of the Qt libraries in order to allow the construction of standalone and self-sufficient executable.<br>
+   ***Requires :*** [Qt Creator][qt-project] preinstalled (prebuilt Qt / MinGW environment), Windows 7, Mac OS 10.6 or Linux<br>
+   Windows PowerShell 3.0 or higher, 7-zip.<br>
+   **Note :** [Qt 5.3][qt-project] or above must be located in C:\Qt. Root directory where the static versions of Qt are installed.<br>
+   By default, use C:\Qt\Static<br>
+   The [7-Zip Command Line Version][7-ZipCLI] located in C:\bin<br>
+   This PowerShell script belongs to Thierry Lelegard. I had to modify it (see the code) cause it doesn't work for me. But now it works.<br>
+   The files location and links are changing, so you'll have to change the script to get it working again in the future.<br>
+   Here is [instruction][qt-static] how to play with it.<br>
    
 * `theClock`: Shows window form with current date and clock. Just demonstrate how to do it in Qt C++<br>
    **Note :** This code is not well optimized. If any tips, write me about.<br>
@@ -58,3 +69,6 @@ send a Pull Request.
 [homepage]:http://dimig.blogspot.com
 [dimig]:http://dimig.blogspot.com
 [qt-project]:http://qt-project.org/downloads
+[QtlMovie]:http://sourceforge.net/p/qtlmovie/code/ci/master/tree/build
+[qt-static]:http://qt-project.org/wiki/How-to-build-a-static-Qt-for-Windows-MinGW
+[7-ZipCLI]:http://www.7-zip.org/download.html
